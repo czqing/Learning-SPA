a96548aa6fcebdbdf3e501d20f62b51e2db7b987
czqing <chenzeqing1990@126.com>
2015-06-27 23:39:04 +0800

    feature: spa.shell

---
+++ spa/js/spa.shell.js
@@ -0,0 +1,26 @@
+/*
+ * spa.shell.js
+ * Shell module for SPA
+ */
+
+/* global $, spa */
+spa.shell = (function () {
+  var
+    configMap = {
+      main_html : String()
+        + '<div class="spa-shell-head">'
+          + '<div class="spa-shell-head-logo"></div>'
+          + '<div class="spa-shell-head-acct"></div>'
+          + '<div class="spa-shell-head-search"></div>'
+        + '</div>'
+        + '<div class="spa-shell-main">'
+          + '<div class="spa-shell-main-nav"></div>'
+          + '<div class="spa-shell-main-content"></div>'
+        + '</div>'
+        + '<div class="spa-shell-foot"></div>'
+        + '<div class="spa-shell-chat"></div>'
+        + '<div class="spa-shell-modal"></div>'
+        + '<div class="">'
+    },
+    
+})
