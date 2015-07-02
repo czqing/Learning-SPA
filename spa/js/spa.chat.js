eaec4c1392f1f75e0e638d55869f83da6a5427e9
czqing <chenzeqing1990@126.com>
2015-07-02 10:59:20 +0800

    feature: chat js

--- spa/js/spa.chat.js
+++ spa/js/spa.chat.js
@@ -9,16 +9,66 @@ spa.chat = (function () {
   var
     configMap = {
       main_html: String()
-        + '<div style="padding:1em; color:#fff;">'
-          + 'Say hello to chat'
+        + '<div class="spa-chat">'
+          + '<div class="spa-chat-head">'
+            + '<div class="spa-chat-head-toggle">+</div>'
+            + '<div class="spa-chat-head-title">'
+              + 'Chat'
+            + '</div>'
+          + '</div>'
+          + '<div class="spa-chat-closer">x</div>'
+          + '<div class="spa-chat-sizer">'
+            + '<div class="spa-chat-msgs"></div>'
+            + '<div class="spa-chat-box">'
+              + '<input type="text">'
+              + '<div>send</div>'
+            + '</div>'
+          + '</div>'
         + '</div>',
-      settable_map: {}
+      settable_map: {
+        slider_open_time: true,
+        slider_close_time: true,
+        slider_opened_em: true,
+        slider_closed_em: true,
+        slider_opened_title: true,
+        slider_closed_title: true,
+
+        chat_model: true,
+        people_model: true,
+        set_chat_anchor: true
+      },
+
+      slider_open_time: 250,
+      slider_close_time: 250,
+      slider_opened_em: 16,
+      slider_closed_em: 2,
+      slider_opened_title: 'Click to close',
+      slider_closed_title: 'Click to open',
+
+      chat_model: null,
+      people_model: null,
+      set_chat_anchor: null
     },
-    stateMap = { $container: null },
+    stateMap = {
+      $append_target: null,
+      position_type: 'closed',
+      px_per_em: 0,
+      slider_hidden_px: 0,
+      slider_closed_px: 0,
+      slider_opened_px: 0
+    },
     jqueryMap = {},
 
-    setJqueryMap, configModule, initModule
+    setJqueryMap, getEmSize, setPxSizes, setSliderPosition,
+    onClickToggle, configModule, initModule
   ;
+
+  //utility method
+  getEmSize = function ( elem ) {
+    return Number(
+      getComputedStyle( elem, '' ).fontSize.match(/\d*\.?\d*/)[0]
+    );
+  };
 
   //DOM method /setJqueryMap/
   setJqueryMap = function () {
