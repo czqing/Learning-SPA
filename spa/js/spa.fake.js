951915ad4e055bb49651f68a6eb77a1d814b4c08
czqing <chenzeqing1990@126.com>
2015-07-14 20:49:17 +0800

    feature: people model

--- spa/js/spa.fake.js
+++ spa/js/spa.fake.js
@@ -4,4 +4,28 @@
  */
 
 /*global $, spa */
-spa.fake = (function () { return {}; }());
+spa.fake = (function () {
+  'use strict';
+  var getPeopleList;
+  getPeopleList = function () {
+    return [
+      {
+        name: 'Betty', _id: 'id_01',
+        css_map: { top: 20, left: 20, 'background-color': 'rgb( 128, 128, 128)' }
+      },
+      {
+        name: 'Mike', _id: 'id_02',
+        css_map: { top: 60, left: 20, 'background-color': 'rgb( 128, 255, 128)' }
+      },
+      {
+        name: 'Pebbles', _id: 'id_03',
+        css_map: { top: 100, left: 20, 'background-color': 'rgb( 128, 192, 192)' }
+      },
+      {
+        name: 'Wilma', _id: 'id_04',
+        css_map: { top: 140, left: 20, 'background-color': 'rgb( 192, 128, 128)' }
+      }
+    ];
+  }; 
+  return { getPeopleList: getPeopleList };
+}());
--- spa/js/spa.js
+++ spa/js/spa.js
@@ -5,7 +5,10 @@
 
 /* global $, spa */
 var spa = (function () {
+  'use strict';
+
   var initModule = function ( $container ) {
+    spa.model.initModule();
     spa.shell.initModule( $container );
   };
 
