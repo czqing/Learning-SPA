09193a5c3e7a9f7a83cf790f75f5e52c341364e3
czqing <chenzeqing1990@126.com>
2015-08-23 20:33:42 +0800

    drag

--- webapp/public/js/spa.avtr.js
+++ webapp/public/js/spa.avtr.js
@@ -61,6 +61,63 @@ spa.avtr = (function () {
       css_map: css_map
     });
 
+    onTapNav = function ( event ) {
+      var css_map,
+        $target = $( event.elem_target ).closest('.spa-avtr-box');
+
+      if ( $target.length === 0 ) { return false; }
+      $target.css({ 'background-color': getRandRgb() });
+      updateAvatar( $target );
+    };
+
+    onHeldstartNav = function ( event ) {
+      var offset_target_map, offset_nav_map,
+        $target = $( event.elem_target ).closest('.spa-avtr-box');
+
+      if ( $target.length === 0 ) { return false; }
+
+      stateMap.$drag_target = $target;
+      offset_target_map = $target.offset();
+      offset_nav_map = jqueryMap.$container.offset();
+
+      offset_target_map.top -= offset_nav_map.top;
+      offset_target_map.left -= offset_nav_map.left;
+
+      stateMap.drag_map = offset_target_map;
+      stateMap.drag_bg_color = $target.css('background-color');
+
+      $target
+        .addClass('spa-x-is-drag')
+        .css('background-color');
+    };
+
+    onHeldmoveNav = function ( event ) {
+      var drag_map = stateMap.drag_map;
+      if ( ! drag_map ) { return false; }
+
+      drag_map.top += event.px_delta_y;
+      drag_map.left += event.px_delta_x;
+
+      stateMap.$drag_target.css({
+        top: drag_map.top,
+        left: drag_map.left
+      });
+    };
+
+    onHeldendNav = function ( event ) {
+      var $drag_target = stateMap.$drag_target;
+      if ( ! $drag_target ) { return false; }
+
+      $drag_target
+        .removeClass('sap-x-is-drag')
+        .css('background-color', stateMap.drag_bg_color);
+
+      stateMap.drag_bg_color = undefined;
+      stateMap.$drag_target = null;
+      stateMap.drag_map = null;
+      updateAvatar( $drag_target );
+    };
+
     //todo
   };
   return {}; }());
